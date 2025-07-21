51e907b2d87f0c4c06f58fdae308eba65e5bc491
Jokha <joker2002187@gmail.com>
2025-07-21 23:26:42 +0400

    Step2: creating a menu deriven

---
+++ BankAccount.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class BankAccount {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
+
+        while (running) {
+            System.out.println("1. Create a new account");
+            System.out.println("2. Deposit money");
+            System.out.println("3. Withdraw money");
+            System.out.println("4. View account details");
+            System.out.println("5. Exit");
+            System.out.print("Choose an option (1-5): ");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // consume newline
+
+            if (choice == 5) {
+                System.out.println("Goodbye!");
+                running = false;
+            } else {
+                System.out.println("This option will be added later.");
+            }
+        }
+
+        scanner.close();
+    }
+}
