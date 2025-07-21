ee28d8a6dafc2e3c317bc5e13c7959df1a544dd9
Jokha <joker2002187@gmail.com>
2025-07-22 00:01:40 +0400

    Step2: creating a menu deriven

--- BankAccount.java
+++ BankAccount.java
@@ -40,6 +40,8 @@ public class BankAccount {
         acc1.showAccountInfo();
         acc2.showAccountInfo();
 
+
+        BankAccount currentAccount = acc1;
         while (running) {
             System.out.println("1. Create a new account");
             System.out.println("2. Deposit money");
@@ -51,11 +53,45 @@ public class BankAccount {
             int choice = scanner.nextInt();
             scanner.nextLine(); // consume newline
 
-            if (choice == 5) {
-                System.out.println("Goodbye!");
-                running = false;
-            } else {
-                System.out.println("This option will be added later.");
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter name: ");
+                    String name = scanner.nextLine();
+                    System.out.print("Enter account number: ");
+                    String number = scanner.nextLine();
+                    System.out.print("Enter starting balance: ");
+                    double bal = scanner.nextDouble();
+                    scanner.nextLine(); // consume newline
+                    currentAccount = new BankAccount(name, number, bal);
+                    System.out.println("Account created.");
+                    break;
+
+                case 2:
+                    System.out.print("Enter amount to deposit: ");
+                    double depositAmt = scanner.nextDouble();
+                    scanner.nextLine(); // consume newline
+                    currentAccount.deposit(depositAmt);
+                    break;
+
+                case 3:
+                    System.out.print("Enter amount to withdraw: ");
+                    double withdrawAmt = scanner.nextDouble();
+                    scanner.nextLine(); // consume newline
+                    currentAccount.withdraw(withdrawAmt);
+                    break;
+
+                case 4:
+                    currentAccount.showAccountInfo();
+                    break;
+
+                case 5:
+                    System.out.println("Goodbye!");
+                    running = false;
+                    break;
+
+                default:
+                    System.out.println("Invalid option.");
             }
         }
 
